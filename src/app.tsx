c884ee678f899df266e79072186c1e7961228245
92hoy <ho_9209@naver.com>
2021-06-16 16:07:25 +0900

    와 되게어렵네git add .

---
+++ src/app.tsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+// import Navigation from './components/Navigation';
+
+// function App() {
+//   return (
+//     <>
+//       <Header title="React App1" />
+//       <Footer title="React App2" />
+//       <Navigation title="React App3" />
+
+//     </>
+//   );
+// }
+
+// export default App;
